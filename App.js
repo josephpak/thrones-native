8503a10a94e1c9788b0e0f18cb57c21a2cb210f8
joepak <josephpak2014@gmail.com>
2019-04-20 15:04:40 -0400

    Adds support for React Native styled components

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import styled from 'styled-components/native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,34 +17,34 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const AppContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #F5FCFF;
+`
+
+const Header = styled.Text`
+  font-size: 20;
+  text-align: center;
+  margin-bottom: 10;
+`
+
+const SubHeader = styled.Text`
+  text-align: center;
+  color: #333333;
+  margin-bottom: 5;
+`
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <AppContainer>
+        <Header>Thrones Watchlist</Header>
+        <SubHeader>To get started, edit App.js</SubHeader>
+        <SubHeader>{instructions}</SubHeader>
+      </AppContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
